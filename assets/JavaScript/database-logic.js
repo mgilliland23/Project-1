c4a8000e2c2ceed1ab7dc811e51dcb8ba243fa78
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-06-14 19:05:31 -0400

    edited favorites sections

--- assets/JavaScript/database-logic.js
+++ assets/JavaScript/database-logic.js
@@ -26,6 +26,7 @@ firebase.auth().onAuthStateChanged(function (user) {
         userID = user.uid;
         console.log("User id: " + userID);
         getSavedRecipes(currentDay);
+        getFavorites()
 
     } else {
         console.log("no one is signed in")
@@ -213,7 +214,6 @@ $(document).on("click", ".toggleFavBut", function () {
     };
     pushFavorites(recipe);
 });
-var userID;
 
 var favorites = "";
 //Save the recipe that the user has selected to the database 
@@ -224,3 +224,81 @@ function pushFavorites(recipe) {
     });
 
 }
+
+var favorites = [];
+//retrieve the current user's recipes from the database for the given day
+ function getFavorites() {
+    //  favorites.forEach(function (favorite) {
+ //   console.log("hope its started building favs")
+    //  firebase.database().ref(userID + "/favorites" + favorite).once('value').then(function (snapshot) {
+    firebase.database().ref(userID + "/favorites").once('value').then(function (snapshot) {
+     //   console.log("hope it got to here")
+        snapshot.forEach(function (child) {
+            //This is the recipe object that we use through out the application
+            var recipe = child.val();
+            console.log(recipe);
+    //        console.log("hope its about to build the card")
+            buildSavedFavoriteCard(recipe);
+            console.log("hope its finished building favs")
+        });
+    });
+    //  })
+}
+
+ function buildSavedFavoriteCard(recipe) {
+    console.log("started building favorite cards")
+    $("#favorite-dump").empty();
+    favorites.forEach(function (recipe, index) {
+        var recipeCard = $("<div>").addClass("card ks-card");
+
+        var img = $("<img>").addClass("card-img-top").attr("src", recipe.image);
+
+        var cardBody = $("<div>").addClass("card-body");
+
+        var title = $("<h5>").addClass("card-title").text(recipe.label);
+
+        var heartButton = $("<button>").addClass("far fa-heart favoriteButton fa-lg toggleFavBut mb-2");
+
+        var recipeButtonHolder = $("<span>").addClass("recipe-link")
+        console.log("recipe url: " + recipe.url);
+        var recipeButton = $("<a>").addClass("ks-button-close").attr("href", recipe.url).attr("target", "_blank").text("Recipe ");
+
+
+        var externalSite = $("<i>").addClass("fas fa-external-link-alt fa-xs");
+
+        var ingredients = $("<ul>").addClass("list-group list-group-flush hiddenIngredientList");
+
+        recipeButtonHolder.append(recipeButton)
+
+        recipe.ingredientLines.forEach(function (ingredient) {
+            console.log("ingredients: " + ingredient);
+            var li = $("<li>").addClass("list-group-item").text(ingredient);
+            ingredients.append(li);
+        });
+
+        var ingredientsButton = $("<button>").addClass("ks-button-recipe ingredientsButtonClick").attr("data-toggle", "modal").attr("data-target", "#ingredientsModal").text("Ingredients");
+        ingredientsButton.on("click", function () {
+            console.log("ingredient button clicked")
+            $(".modalDump").empty();
+            $(".recipe-dump").empty(); //recipe link
+            var clickedRecipeCard = $(this).parent();
+
+            console.log(ingredients);
+            $(".modalDump").html(ingredients);
+            $(".modalDump").find(".hiddenIngredientList").removeClass("hiddenIngredientList");
+            var recipeButtonHolder = clickedRecipeCard.find(".recipe-link").html(); //recipe link
+            console.log("recipe link " + recipe.url); //recipe link
+            $(".recipe-dump").html(recipeButtonHolder); //recipe link
+        });
+
+        cardBody.append(title, heartButton, ingredients, ingredientsButton, recipeButtonHolder);
+
+        recipeButton.append(externalSite);
+
+        recipeCard.append(img, cardBody);
+
+        // Appending to page
+
+        $("#favorite-dump").append(recipeCard);
+    });
+} 
